c89f6235c6bde0b7c2dd19f64dc640a945d61cd0
bernardomerlo <bernardomerlo49@gmail.com>
2026-05-29 21:45:10 -0300

    teste

--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,7 @@ Route::post('/transacoes', [TransacaoController::class, 'store']);
 Route::put('/transacoes/{id}', [TransacaoController::class, 'update']);
 Route::delete('/transacoes/{id}', [TransacaoController::class, 'destroy']);
 Route::get('/meses/{ano_mes}', [MesController::class, 'show']);
+Route::get('/teste', function () {
+    return response()->json(['message' => 'API is working']);
+});
 Route::post('/meses/{ano_mes}/config', [MesController::class, 'updateConfig']);
